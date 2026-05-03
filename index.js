67d782b18192ac808cd6369708e072f3c8eab9c9
Nimra456566 <nimraarif785@gmail.com>
2026-05-03 12:49:24 +0500

    sloving error

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+const connectDB = require("./database/mongodb.config.js");
+connectDB();
+
+console.log("URI:", process.env.MONGO_URI);
+
 const app = express();
-const PORT= 3000;
-const boardRoutes = require("./routes/board.routes.js");
+const PORT = 3000;
+//const boardRoutes = require("./routes/board.routes.js");
 app.use(express.json());
 
-app.use("/boards", boardRoutes);
+//app.use("/boards", boardRoutes);
 app.get("/", (req, res) => {
   res.send("server running");
 });
